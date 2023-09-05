28a93c818c17f446137de615ec68702d642cdacb
mateotrucco <mateotrucco.arg@gmail.com>
2023-09-05 17:09:19 -0300

    actu calculadora de i

---
+++ python/Casino/menu.py
@@ -0,0 +1,10 @@
+from tkinter import * 
+
+root = Tk()
+root.title("Casino Virtual __FALSO__")
+root.attributes('-fullscreen', True)
+
+games_btn = Button(root, text="Play", command=lambda)
+
+
+root.mainloop()
---
+++ python/botDiscord.py
@@ -0,0 +1,3 @@
+a = 6
+
+print("-5" if a < 5 else "+5")
