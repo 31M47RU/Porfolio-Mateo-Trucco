3c888944c9ca8911be492abf4cfd21f94078b2dd
mateotrucco <mateotrucco.arg@gmail.com>
2023-09-15 05:46:24 -0300

    calculadora para los cromos de steam

--- python/Casino/menu.py
+++ python/Casino/menu.py
@@ -1,10 +1,10 @@
-from tkinter import * 
+import tkinter as tk
 
-root = Tk()
+root = tk.Tk()
 root.title("Casino Virtual __FALSO__")
 root.attributes('-fullscreen', True)
 
-games_btn = Button(root, text="Play", command=lambda)
+games_btn = tk.Button(root, text="Play")
 
 
 root.mainloop()
